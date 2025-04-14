8a196ae26a38ba4f2570075baf1cd8279dce015b
thespielplatz <informatics@gmx.net>
2025-04-14 21:47:26 +0200

    fix: show - with negative sats

--- lib/pages/wallet/transaction_details/get_formatted_sats.dart
+++ lib/pages/wallet/transaction_details/get_formatted_sats.dart
@@ -4,13 +4,16 @@ import 'package:volt_ui/models/lndhub/lndhub_transaction.dart';
 String getFormattedSatsFromTransaction(LndHubTransaction transaction,
     {bool addPlusSign = true}) {
   return getFormattedSats(
-    transaction.transactionType == LndHubTransactionType.payment
-        ? -transaction.value
-        : transaction.value,
+    transaction.value,
     addPlusSign: addPlusSign,
   );
 }
 
 String getFormattedSats(int value, {bool addPlusSign = true}) {
-  return '${value > 0 && addPlusSign ? '+ ' : ''}${SatsInputFormatter.format(value.toString())} sats';
+  String sign = value < 0
+      ? '- '
+      : addPlusSign
+          ? '+ '
+          : '';
+  return '$sign${SatsInputFormatter.format(value.toString())} sats';
 }
