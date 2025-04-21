4879038993f5a2847a83e94e3893c02006bc8b84
thespielplatz <informatics@gmx.net>
2025-04-21 22:21:20 +0200

    chore: add lnurl recognition and decryption

--- lib/pages/wallet/scanner/qr_scanner_page.dart
+++ lib/pages/wallet/scanner/qr_scanner_page.dart
@@ -5,9 +5,10 @@ import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:volt_ui/layout/show_error.dart';
 import 'package:volt_ui/models/lndhub/lndhub_decoded_invoice.dart';
 import 'package:volt_ui/repository/wallet_repository.dart';
+import 'package:dart_lnurl/dart_lnurl.dart';
 import 'package:volt_ui/services/lightning/validate_bolt11.dart';
+import 'package:volt_ui/ui/app_colors.dart';
 
-/// Implementation of Mobile Scanner example with simple configuration
 class QRScannerPage extends StatefulWidget {
   final WalletRepository walletRepository;
   final void Function({String invoice, LndHubDecodedInvoice decodedInvoice})
@@ -24,6 +25,8 @@ class QRScannerPage extends StatefulWidget {
 
 class _QRScannerPageState extends State<QRScannerPage>
     with WidgetsBindingObserver {
+  bool _isLoading = false;
+
   final MobileScannerController controller = MobileScannerController(
     autoStart: false,
     detectionSpeed: DetectionSpeed.normal,
@@ -39,6 +42,7 @@ class _QRScannerPageState extends State<QRScannerPage>
 
     // Start the scanner
     unawaited(controller.start());
+    _isLoading = false;
   }
 
   @override
@@ -77,6 +81,9 @@ class _QRScannerPageState extends State<QRScannerPage>
       return;
     }
 
+    setState(() {
+      _isLoading = true;
+    });
     if (validateBolt11(code)) {
       try {
         LndHubDecodedInvoice decodedInvoice =
@@ -87,19 +94,111 @@ class _QRScannerPageState extends State<QRScannerPage>
         );
         return;
       } catch (e) {
+        setState(() {
+          _isLoading = false;
+        });
         showError(
           context: context,
           text: 'Error processing invoice: $e',
         );
+        return;
       }
+    }
+
+    try {
+      // dart_lnurl can decode lighting: also
+      LNURLParseResult result = await getParams(code);
+      if (result.error != null) {
+        showError(
+          context: context,
+          text:
+              'Error processing lnurl: ${result.error?.status} ${result.error?.reason} ${result.error?.domain} ${result.error?.url}',
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      if (result.withdrawalParams != null) {
+        showError(
+          context: context,
+          text: 'LNURLw not implemented yet',
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      if (result.payParams != null) {
+        showError(
+          context: context,
+          text: 'LNURLp not implemented yet',
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      if (result.authParams != null) {
+        showError(
+          context: context,
+          text: 'LNURLauth not implemented yet',
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      if (result.channelParams != null) {
+        showError(
+          context: context,
+          text: 'LNURLchannel not implemented yet',
+        );
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    } catch (e) {
+      showError(
+        context: context,
+        text: 'LNURL processing error: $e',
+      );
+      setState(() {
+        _isLoading = false;
+      });
+      return;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MobileScanner(
-      onDetect: _handleBarcode,
-      controller: controller,
+    return Stack(
+      children: [
+        MobileScanner(
+          controller: controller,
+          onDetect: _handleBarcode,
+        ),
+
+        // Top-centered loader
+        if (_isLoading)
+          const Align(
+            alignment: Alignment.topCenter,
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: CircularProgressIndicator(
+                  strokeWidth: 5,
+                  color: AppColors.accentColor,
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
